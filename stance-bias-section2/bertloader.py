238b7ad885d3c039503b793f4eca7af87a824c28
Farzam <farzam.fanni@gmail.com>
2021-09-29 15:16:35 +0200

    small fixes

--- stance-bias-section2/bertloader.py
+++ stance-bias-section2/bertloader.py
@@ -23,7 +23,7 @@ FOLDER_NAME = {"16se" : "16semeval",
                 "PImPo":"PImPo"
             }[params.dataset_name]
 
-DATA_PATH = os.path.join(basepath, 'data', FOLDER_NAME, 'data_new.json')
+DATA_PATH = os.path.join(basepath, 'data', FOLDER_NAME, 'data.json')
 
 
 class StanceDataset:
